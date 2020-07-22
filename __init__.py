4ca0899c0435978fdd00ae4acf9cc452c5b2e207
Kris Gesling <kris.gesling@mycroft.ai>
2020-07-22 12:46:29 +0930

    generate speech as required and allow stop

--- __init__.py
+++ __init__.py
@@ -16,37 +16,51 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.audio import wait_while_speaking
 
 
 class Count(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.stop_requested = False
 
     @intent_file_handler('count.intent')
     def handle_count(self, message):
+        self.stop_requested = False
         try:
             number = int(message.data.get("number"))
             response = {'number': message.data.get("number")}
             self.speak_dialog("count_start", data=response)
             for i in range(1, number+1, +1):
+                if self.stop_requested:
+                    break
                 self.speak(str(i) + " .")
-            self.speak_dialog("count_stop")
-            pass
+                wait_while_speaking()
+            if not self.stop_requested:
+                self.speak_dialog("count_stop")
         except:
             self.speak_dialog("count_error")
 
     @intent_file_handler('countdown.intent')
     def handle_countdown_intent(self, message):
+        self.stop_requested = False
         try:
             number = int(message.data.get("number"))
             response = {'number': message.data.get("number")}
             self.speak_dialog("countdown_start", data=response)
             for i in range(number, 0, -1):
+                if self.stop_requested:
+                    break
                 self.speak(str(i) + " .")
-            self.speak_dialog("countdown_stop")
-            pass
+                wait_while_speaking()
+            if not self.stop_requested:
+                self.speak_dialog("countdown_stop")
         except:
             self.speak_dialog("count_error")
+
+    def stop(self):
+        self.stop_requested = True
+
 
 
 def create_skill():
